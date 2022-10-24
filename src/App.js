6061a4b26a067fbf0ead9df66c38c2014bd5bbd6
MiaadEstakhri <miaad8104@gmail.com>
2022-10-24 22:39:11 +0330

    saved Contacts in localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import AddContact from "./components/AddContact/AddContact";
 import ContactList from "./components/ContactList/ContactList";
@@ -24,6 +24,16 @@ function App() {
     const filteredContacts = contacts.filter((e) => e.id !== id);
     setContacts(filteredContacts);
   };
+
+  useEffect(() => {
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (savedContacts) setContacts(savedContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <main className="App">
       <h1>Contact App</h1>
